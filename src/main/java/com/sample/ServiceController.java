a5c503c8491412f4bfd52ed98671fabe8885b8c1
Свалов Дмитрий <svalovdmitrii@mail.ru>
2018-08-27 03:32:52 +0500

    Настроил проект, запустил

--- src/main/java/com/sample/ServiceController.java
+++ src/main/java/com/sample/ServiceController.java
@@ -2,12 +2,14 @@ package com.sample;
 
 import com.sample.model.BankRequest;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/bankRequest")
 public class ServiceController {
 
-    @RequestMapping("/greeting")
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
     public BankRequest request() {
         return new BankRequest(1, "Вася");
     }
